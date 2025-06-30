5bc0d3f657df295ac60e2aabfc75d5d1fe1aedb3
bruceoaudo <bruceodoyoaudo@gmail.com>
2025-06-30 23:44:00 +0300

    Add electron to the project

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const serverPort = Number(process.env.SERVER_PORT);
 const serverHost = process.env.SERVER_HOST;
 const localPort = Number(process.env.LOCAL_PORT);
 
-let selectedPeer;
 
 const client = net.createConnection(
   { host: serverHost, port: serverPort, localPort: localPort },
@@ -25,38 +24,7 @@ const client = net.createConnection(
 client.on("data", (data) => {
   const peers = JSON.parse(data.toString()).peers;
 
-  if (Array.isArray(peers) && peers.length > 0) {
-    const randomIndex = Math.floor(Math.random() * peers.length);
-    selectedPeer = peers[randomIndex];
-
-    const peerHost = selectedPeer.ip;
-
-    const peerConnection = net.createConnection(
-      {
-        host: peerHost,
-        port: parseInt(localPort, 10),
-      },
-      () => {
-        const message = `Hello from ${client.localAddress}:${client.localPort}`;
-        console.log("Sending to peer:", message);
-        peerConnection.write(message + "\n");
-      }
-    );
-
-    peerConnection.on("data", (peerData) => {
-      console.log("Response from peer:", peerData.toString());
-    });
-
-    peerConnection.on("end", () => {
-      console.log("Disconnected from peer.");
-    });
-
-    peerConnection.on("error", (err) => {
-      console.error("Error connecting to peer:", err.message);
-    });
-  } else {
-    console.log("No peers available to select.");
-  }
+  console.log(data.toString())
 
   client.end();
 });
@@ -64,3 +32,4 @@ client.on("data", (data) => {
 client.on("end", () => {
   console.log("disconnected from server");
 });
+
---
+++ main.js
@@ -0,0 +1,22 @@
+import { app, BrowserWindow } from "electron";
+
+const CreateWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile("index.html");
+};
+
+app.whenReady().then(() => {
+  CreateWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
